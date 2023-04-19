58f878e2050c5e53228e4d74a5ab6872736f87c0
TheVitorGomez <7vitorgomez@gmail.com>
2023-04-19 16:48:45 -0300

    nova função: conta afundados

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ def preenche_frota(frota: dict, nome_navio: str, linha: int, coluna: int, orient
 
     return frota
 
+
 def faz_jogada(tabuleiro: list[list], linha: int, coluna: int) -> list[list]:
     if tabuleiro[linha][coluna] == 1:
         tabuleiro[linha][coluna] = 'X'
@@ -24,22 +25,37 @@ def faz_jogada(tabuleiro: list[list], linha: int, coluna: int) -> list[list]:
 
     return tabuleiro
 
-def posiciona_frota (frota: dict) -> list:
+
+def posiciona_frota(frota: dict) -> list:
     tabuleiro = [
-      [0,0,0,0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0,0,0,0],
-  ]
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ]
     for i in frota.values():
         for j in i:
             for k in j:
                 tabuleiro[k[0]][k[1]] = 1
 
     return tabuleiro
+
+
+def afundados(frota: dict, tabuleiro: list) -> int:
+    n_afundados = 0
+    for f in frota.values():
+        for cordenadas in f:
+            count = 0
+            for cordenada in cordenadas:
+                if tabuleiro[cordenada[0]][cordenada[1]] == 'X':
+                    count += 1
+            if count == len(cordenadas):
+                n_afundados += 1
+
+    return n_afundados
